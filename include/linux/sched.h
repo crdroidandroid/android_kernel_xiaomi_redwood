511801eb066658a75e79d388f0da43e4dd74f586
Thomas Gleixner <tglx@linutronix.de>
2020-11-18 20:48:43 +0100

    sched: highmem: Store local kmaps in task struct
    Instead of storing the map per CPU provide and use per task storage. That
    prepares for local kmaps which are preemptible.
    
    The context switch code is preparatory and not yet in use because
    kmap_atomic() runs with preemption disabled. Will be made usable in the
    next step.
    
    The context switch logic is safe even when an interrupt happens after
    clearing or before restoring the kmaps. The kmap index in task struct is
    not modified so any nesting kmap in an interrupt will use unused indices
    and on return the counter is the same as before.
    
    Also add an assert into the return to user space code. Going back to user
    space with an active kmap local is a nono.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Acked-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lore.kernel.org/r/20201118204007.372935758@linutronix.de
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -31,6 +31,7 @@
 #include <linux/task_io_accounting.h>
 #include <linux/posix-timers.h>
 #include <linux/rseq.h>
+#include <asm/kmap_size.h>
 #include <linux/android_kabi.h>
 #include <linux/android_vendor.h>
 
@@ -675,6 +676,13 @@ enum perf_event_task_context {
 
 struct wake_q_node {
 	struct wake_q_node *next;
+};
+
+struct kmap_ctrl {
+#ifdef CONFIG_KMAP_LOCAL
+	int				idx;
+	pte_t				pteval[KM_MAX_IDX];
+#endif
 };
 
 struct task_struct {
@@ -1338,6 +1346,7 @@ struct task_struct {
 	unsigned int			sequential_io;
 	unsigned int			sequential_io_avg;
 #endif
+	struct kmap_ctrl		kmap_ctrl;
 #ifdef CONFIG_DEBUG_ATOMIC_SLEEP
 	unsigned long			task_state_change;
 #endif

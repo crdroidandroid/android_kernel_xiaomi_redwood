4ec4407224e9f19691471eb52c464a9dd5eb8bf5
Taniya Das <tdas@codeaurora.org>
2021-06-09 23:06:48 +0530

    cpufreq: qcom-hw: Deregister the cooling device
    Unregister the cooling device during cpu hotplug.
    
    Change-Id: I09a6a629c5c65ac8f1edc41503b342006f502375
    Signed-off-by: Taniya Das <tdas@codeaurora.org>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/cpufreq/qcom-cpufreq-hw.c
+++ drivers/cpufreq/qcom-cpufreq-hw.c
@@ -114,6 +114,7 @@ static const u16 cpufreq_qcom_epss_std_offsets[REG_ARRAY_SIZE] = {
 
 static struct cpufreq_qcom *qcom_freq_domain_map[NR_CPUS];
 static struct cpufreq_counter qcom_cpufreq_counter[NR_CPUS];
+static struct thermal_cooling_device *cdev[NR_CPUS];
 
 static unsigned int qcom_cpufreq_hw_get(unsigned int cpu);
 
@@ -421,7 +422,6 @@ static struct freq_attr *qcom_cpufreq_hw_attr[] = {
 
 static void qcom_cpufreq_ready(struct cpufreq_policy *policy)
 {
-	static struct thermal_cooling_device *cdev[NR_CPUS];
 	struct device_node *np;
 	unsigned int cpu = policy->cpu;
 
@@ -446,6 +446,20 @@ static void qcom_cpufreq_ready(struct cpufreq_policy *policy)
 	}
 
 	of_node_put(np);
+}
+
+static int qcom_cpufreq_exit(struct cpufreq_policy *policy)
+{
+	unsigned int cpu = policy->cpu;
+
+	if (!cdev[cpu])
+		return 0;
+
+	cpufreq_cooling_unregister(cdev[cpu]);
+
+	cdev[cpu] = NULL;
+
+	return 0;
 }
 
 static int qcom_cpufreq_hw_cpu_online(struct cpufreq_policy *policy)
@@ -553,6 +567,7 @@ static struct cpufreq_driver cpufreq_qcom_hw_driver = {
 	.offline	= qcom_cpufreq_hw_cpu_offline,
 	.suspend	= qcom_cpufreq_hw_suspend,
 	.resume		= qcom_cpufreq_hw_resume,
+	.exit		= qcom_cpufreq_exit,
 };
 
 static int cpuhp_qcom_online(unsigned int cpu)

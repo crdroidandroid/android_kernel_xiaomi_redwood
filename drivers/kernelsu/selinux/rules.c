06ac6d632f6ba8beb924f0bf5c4c02ecca2f3026
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2025-08-01 00:54:50 +0600

    kernelsu: selinux: rules: Remove unnecessary RCU dereference in get_policydb()
    get_policydb() uses rcu_dereference() to read pointers to selinux_state.policy
    / selinux_state.ss.
    
    But in the SELinux implementation, these pointers are assigned once during
    initialization and never changed with rcu_assign_pointer(), rendering the
    rcu_dereference() call in get_policydb() completely useless. This just adds
    unwanted overhead and implies concurrency pattern that is not even present in
    the kernel.
    
    Therefore, read the pointers directly since it's safe.
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/kernelsu/selinux/rules.c
+++ drivers/kernelsu/selinux/rules.c
@@ -23,10 +23,10 @@ static struct policydb *get_policydb(void)
 	struct policydb *db;
 // selinux_state does not exists before 4.19
 #ifdef SELINUX_POLICY_INSTEAD_SELINUX_SS
-	struct selinux_policy *policy = rcu_dereference(selinux_state.policy);
+	struct selinux_policy *policy = selinux_state.policy;
 	db = &policy->policydb;
 #else
-	struct selinux_ss *ss = rcu_dereference(selinux_state.ss);
+	struct selinux_ss *ss = selinux_state.ss;
 	db = &ss->policydb;
 #endif
 	return db;

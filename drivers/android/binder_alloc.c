d2e6c8c3802a804eb9c35dd35e760967e51d7603
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2025-08-02 00:06:51 +0600

    Revert "binder_alloc: Fix illegal highmem page mapping in atomic context"
    This reverts commit 23eb704152da478bc40ed498ba00d7b7ff4cab0f.
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/android/binder_alloc.c
+++ drivers/android/binder_alloc.c
@@ -803,9 +803,9 @@ static void binder_alloc_clear_buf(struct binder_alloc *alloc,
 		page = binder_alloc_get_page(alloc, buffer,
 					     buffer_offset, &pgoff);
 		size = min_t(size_t, bytes, PAGE_SIZE - pgoff);
-		kptr = kmap_atomic(page) + pgoff;
+		kptr = kmap(page) + pgoff;
 		memset(kptr, 0, size);
-		kunmap_atomic(kptr);
+		kunmap(page);
 		bytes -= size;
 		buffer_offset += size;
 	}
